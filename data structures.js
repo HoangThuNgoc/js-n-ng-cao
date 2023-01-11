7d4cc092ed06f4ca60136085fb2c91b184cf966f
HoangThuNgoc <hoangthungoc11h@gmail.com>
2023-01-11 19:11:22 +0700

    update code

--- data structures.js
+++ data structures.js
@@ -4,15 +4,12 @@ const restaurant={
     categories:['chicken','organic'],
     starteMenu:['sup','banh','keo'],
     mainMenu:['pizza','cake','pasta'],
-
 }
 
-const arr=[1,2, ...[3,4]];
-
+const arr=[1,2, ...[3,4]]
 
 const[a,b,...others]=[1,2,3,4,5];
 console.log[a,b,others];
-
 
 const [pizza, ,pasta, ...otherFood]=[
     ...restaurant.mainMenu,
